22aaad25ec0c86d1afd34427fcc82d7924fc0efc
Venancio Bastos <vebastos@c1r7s9.42porto.com>
2026-05-29 19:24:32 +0100

    day done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This project has been created as part of the 42 curriculum by vebastos
+
+
+
+
+Memory problems solution
+
+export UV_CACHE_DIR=~/sgoinfre/uv_cache
+export UV_PROJECT_ENVIRONMENT=~/sgoinfre/venv
+export TRANSFORMERS_CACHE=/sgoinfre/vebastos/huggingface
+export HF_HOME=/sgoinfre/vebastos/huggingface
--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,14 @@
-from llm_sdk.llm_sdk import Small_LLM_Model
+from llm_sdk import Small_LLM_Model
+import numpy as np
 
 
 def main() -> None:
     model = Small_LLM_Model()
-    tokens = model.encode("Olá, tudo bem?")
-    print(tokens)
+    tokens_tensor = model.encode("What is the sum of 40 and 2?")
+    tokens_list = tokens_tensor.tolist()[0]
+    logist_list = np.array(model.get_logits_from_input_ids(tokens_list))
+    print(f"tokens_list: {tokens_list}")
+    print(f"logist_list: {logist_list}")
 
 
 if __name__ == "__main__":
